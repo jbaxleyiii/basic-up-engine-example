de3a4943668a608f6511b980bea193a398c04694
James Baxley <james@meteor.com>
2018-02-14 14:27:15 -0500

    YOU ONLY GET GRAPHIQL

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { graphqlExpress } from "apollo-server-express";
+import { graphqlExpress, graphiqlExpress } from "apollo-server-express";
 import bodyParser from "body-parser";
 
 import { schema } from "./schema";
@@ -9,6 +9,10 @@ const server = express();
 
 // allow for any url to be GraphQL since this is a lambda function
 server.post("*", bodyParser.json(), graphqlExpress({ schema, tracing: true }));
+server.get("*", (...args) => {
+  const [req] = args;
+  return graphiqlExpress({ endpointURL: req.path })(...args);
+});
 
 server.listen(PORT, () => {
   console.log(`GraphQL Server is now running on http://localhost:${PORT}`);
